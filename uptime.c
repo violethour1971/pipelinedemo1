0ac8d2082c7fb2e5653b41e143dfbb28c73598c2
Michael Martinez <violethour1971@gmail.com>
2021-11-08 16:20:42 +0000

    add some code and some build steps

---
+++ uptime.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <errno.h>
+#include <linux/unistd.h>       /* for _syscallX macros/related stuff */
+#include <linux/kernel.h>       /* for struct sysinfo */
+#include <sys/sysinfo.h>
+
+long get_uptime()
+{
+    struct sysinfo s_info;
+    int error = sysinfo(&s_info);
+    if(error != 0)
+    {
+        printf("code error = %d\n", error);
+    }
+    return s_info.uptime;
+}
+
+
+int main()
+{
+    printf("Uptime results: %ld\n", get_uptime());
+    return 0;
+}
+
